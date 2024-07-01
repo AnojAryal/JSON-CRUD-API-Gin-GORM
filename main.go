985a9d2d624c410ca8ad481c4cf10613f3490264
AnojAryal <anoj1810@gmail.com>
2024-07-01 13:34:24 +0545

    starting point for go-app setup

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "HI setup completed. we are good to Go...",
 		})
 	})
 	r.Run()
